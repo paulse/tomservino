de34a57ef07b5eae30ccd79c3ca5d5503ccfbda8
paulse <paulse@gmail.com>
2024-11-25 14:21:46 -0800

    updates

--- src/pages/contact/examples.js
+++ src/pages/contact/examples.js
@@ -12,7 +12,7 @@ export default class Index extends React.Component {
               <h1>Hi people</h1>
               <p>
                 This is an example site integrating Netlify’s form handling with
-                Gatsby
+                Gatsby bleh blewh
               </p>
               <ul>
                 <li>
